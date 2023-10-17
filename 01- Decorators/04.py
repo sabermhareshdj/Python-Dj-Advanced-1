1ad7e41926e67eb1731a861de6e7e6a5cc7acf27
Saber Mharesh <dj.saber.mharesh@gmail.com>
2023-10-17 15:23:21 +0300

    04.py execution time

---
+++ 01- Decorators/04.py
@@ -0,0 +1,16 @@
+import time
+def calculate_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func() 
+        end_time = time.time()
+        
+        print(f'execution time = { end_time - start_time }')
+    return wrapper
+
+@calculate_execution_time
+def myfunc():
+    for x in range(10000000):
+        pass
+    
+myfunc()
